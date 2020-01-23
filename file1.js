16ceb7687b83e22fb933cb0de8104ea9b1d3f327
Priti Patel <pdpatel2@gmail.com>
2020-01-23 13:29:43 -0600

    code demo

---
+++ file1.js
@@ -0,0 +1,14 @@
+const sum = (num1, num2) => {
+  return num1 + num2
+}
+
+console.log('between sum and multiply')
+
+const multiply = (num1, num2) => {
+  return num1 * num2
+}
+
+module.exports = {
+  sum,
+  multiply
+}
---
+++ file2.js
@@ -0,0 +1,7 @@
+const {sum, multiply} = require('./file1.js')
+
+const mySum = sum(100, 101)
+const myProduct = multiply(4, 8)
+
+console.log('mySum', mySum)
+console.log('myProduct', myProduct)
